d78486fad1c4a7d1174dd327aa1d1e74216c8932
Chris Hung <yinye1986@gmail.com>
2024-08-04 01:43:15 +0800

    bak

--- stm32f103c8t6/main.c
+++ stm32f103c8t6/main.c
@@ -1,15 +1,17 @@
 #include "stm32f10x.h"
 #include "OLED.h"
-#include "Delay.h"
+#include "delay.h"
 #include "Serialaa.h"
 #include "key.h"
 
-uint8_t RxBuffer[32];
+uint8_t RxBuffer[32]={56,14,4e,1e,02,b0,27,09,00,87,27,10,00,4c,00,46,00,26,00,23,00,19,00,1c,00,13,00,15,00,10,00,0f};
+uint8_t status;
 char AVolt[6];
 char prevAVolt[6];
 char AAmps[6];
 char prevAAmps[6];
-uint8_t status;
+uint8_t zashu
+//pf功率因数 Vrms Irms
 
 uint8_t hasChanged(char *prestr, char *str) {
     for(uint8_t i=0;i<6;i++) {
@@ -28,21 +30,18 @@ void hextochar(uint8_t qwf, uint8_t arst, char *str) {
         return;
     }
 
-    // 记录字符位置
     char temp[6]; // 足够存储65535的5位数字和结束符
     int i = 0;
 
-    // 将数字从末尾开始转换为字符
     while (u16arg > 0) {
-        temp[i++] = (u16arg % 10) + '0'; // 将最后一位数字转换为字符
-        u16arg /= 10; // 去掉最后一位数字
+        temp[i++] = (u16arg % 10) + '0';
+        u16arg /= 10;
     }
 
-    // 反转字符串
     for (int j = 0; j < i; j++) {
         str[j] = temp[i - j - 1];
     }
-    str[i] = '\0'; // 添加字符串结束符
+    str[i] = '\0';
 }
 
 void GPIO_IN_FLOATING_Config(void) {
@@ -88,7 +87,7 @@ int main(void) {
     TIM2_IRQHandler();
     OLED_Init();
     GPIO_Init_Buttons();
-    USART1_Init(9600);
+//    USART1_Init(9600);
     Delay_ms(25);
     OLED_Clear();
     status=0;
@@ -101,47 +100,46 @@ int main(void) {
 	while(1) {
         switch(status) {
             case 1:
-                hextochar(0, 1, AVolt);
-                hextochar(2, 3, AAmps);
-                if (hasChanged(prevAVolt,AVolt)==1) {
-                    OLED_ShowChar(3,4,AVolt[0]);
-                    OLED_ShowChar(3,5,AVolt[1]);
-                    OLED_ShowChar(3,6,AVolt[2]);
-                    OLED_ShowChar(3,7,'.');
-                    OLED_ShowChar(3,8,AVolt[3]);
-                    OLED_ShowChar(3,9,AVolt[4]);
-
-                    OLED_ShowChar(4,4,AAmps[0]);
-                    OLED_ShowChar(4,5,'.');
-                    OLED_ShowChar(4,6,AAmps[1]);
-                    OLED_ShowChar(4,7,AAmps[2]);
-                    OLED_ShowChar(4,8,AAmps[3]);
-                    OLED_ShowChar(4,9,AAmps[4]);
-                    OLED_Clear();
-                    OLED_ShowString(2,1,"F:");
-                    OLED_ShowString(2,3,"50.02 Hz");
-                    OLED_ShowString(3,1,"LV:");
-                    OLED_ShowString(4,1,"LC:");
-
-                    for(int i=0;i<6;i++) {
-                        prevAVolt[i] = AVolt[i];
-                        prevAAmps[i] = AAmps[i];
-                    }
-                }
-
                 break;
             case 2:
                 break;
             case 3:
                 break;
             default:
-                OLED_ShowString(1,1,"press A to START");
-                OLED_ShowString(2,1,"  groupMember:  ");
-                OLED_ShowString(3,1,"  BZJ  DLW  HCZ ");
-                OLED_ShowString(4,1,"   From  WXIT   ");
-                Delay_ms(500);
-        }
-    }
+                while(a) {
+                    uint8_t a=1;
+                    OLED_ShowString(1,1,"press A to START");
+                    OLED_ShowString(2,1,"  groupMember:  ");
+                    OLED_ShowString(3,1,"  BZJ  DLW  HCZ ");
+                    OLED_ShowString(4,1,"   From  WXIT   ");
+                    a=0;
+                }
+                break;
 }
 
-
+//                hextochar(0, 1, AVolt);
+//                hextochar(2, 3, AAmps);
+//                if (hasChanged(prevAVolt,AVolt)==1) {
+//                    OLED_ShowChar(3,4,AVolt[0]);
+//                    OLED_ShowChar(3,5,AVolt[1]);
+//                    OLED_ShowChar(3,6,AVolt[2]);
+//                    OLED_ShowChar(3,7,'.');
+//                    OLED_ShowChar(3,8,AVolt[3]);
+//                    OLED_ShowChar(3,9,AVolt[4]);
+//                } else if(hasChanged(prevAVolt,AVolt)==1) {
+//                    OLED_ShowChar(4,4,AAmps[0]);
+//                    OLED_ShowChar(4,5,'.');
+//                    OLED_ShowChar(4,6,AAmps[1]);
+//                    OLED_ShowChar(4,7,AAmps[2]);
+//                    OLED_ShowChar(4,8,AAmps[3]);
+//                    OLED_ShowChar(4,9,AAmps[4]);
+//                    OLED_Clear();
+//                    OLED_ShowString(2,1,"F:");
+//                    OLED_ShowString(2,3,"50.02 Hz");
+//                    OLED_ShowString(3,1,"LV:");
+//                    OLED_ShowString(4,1,"LC:");
+//
+//                    for(int i=0;i<6;i++) {
+//                        prevAVolt[i] = AVolt[i];
+//                        prevAAmps[i] = AAmps[i];
+//
